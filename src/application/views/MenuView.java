eb84c38562d08745efcae2883cb45818fed08b27
mhdjauar <mhdjanuar.17@gmail.com>
2025-04-21 18:08:33 +0700

    fixing: menu view

--- src/application/views/MenuView.java
+++ src/application/views/MenuView.java
@@ -399,7 +399,7 @@ public class MenuView extends javax.swing.JFrame {
         MainContent.revalidate();
 
         // add Panel, add panel
-        MainContent.add(new AkunView(loggedInUser));
+        MainContent.add(new ListDataView());
         MainContent.repaint();
         MainContent.revalidate();
     }//GEN-LAST:event_listDataMouseClicked
